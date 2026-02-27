6c2ae7990c42087b78ea9057aa296950feeb6e45
evanfenn <evanfenn@udel.edu>
2026-02-27 17:33:41 -0500

    Updated last function body

--- src/objects.ts
+++ src/objects.ts
@@ -147,11 +147,6 @@ export function addOption(question: Question, newOption: string): Question {
  * Notice that the second Question is provided as just an object with a `points`
  * field; but the function call would be the same as if it were a `Question` type!
  */
-export function mergeQuestion(
-    id: number,
-    name: string,
-    contentQuestion: Question,
-    { points }: { points: number }
-): Question {
-    return contentQuestion;
+export function mergeQuestion(id: number, name: string, contentQuestion: Question, { points }: { points: number }): Question {
+    return {...contentQuestion, id: id, name: name, points: points, published: false};
 }
