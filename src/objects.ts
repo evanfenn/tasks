a6b5c9aca331416db883d881a88f1e5a21313d9c
evanfenn <evanfenn@udel.edu>
2026-02-27 16:54:42 -0500

    Updated third function body and finished fourth function body

--- src/objects.ts
+++ src/objects.ts
@@ -45,11 +45,11 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    if (question.type === "multiple_choice_question"){
-        return question.options.includes(answer);
+    if (question.type === "short_answer_question"){
+        return true;
     }
     else {
-        return false;
+        return question.options.includes(answer);
     }
 }
 
@@ -60,7 +60,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return question.id.toString() + ": " + question.name.slice(0, 10);
 }
 
 /**
