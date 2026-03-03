a7c0d27b2701b7edab9a4ad89a7d3bd385a2d220
evanfenn <evanfenn@udel.edu>
2026-03-02 19:35:07 -0500

    Added getNonEmptyQuestions body

--- src/nested.ts
+++ src/nested.ts
@@ -15,8 +15,7 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    
-    return [];
+    return questions.filter((question) => !(question.body === "" && question.expected === "" && question.options.length === 0));
 }
 
 /***
