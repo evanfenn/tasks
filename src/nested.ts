0af303d05989c06799fba08f048a68a6a0fb6803
evanfenn <evanfenn@udel.edu>
2026-03-02 21:08:56 -0500

    Added sumPoints body

--- src/nested.ts
+++ src/nested.ts
@@ -47,7 +47,7 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce((sum, question) => sum + question.points, 0);
 }
 
 /***
