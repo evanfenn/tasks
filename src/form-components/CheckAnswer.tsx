64dac0dfaee1f9572a3e3873f6eb61862f7f33a3
evanfenn <evanfenn@udel.edu>
2026-03-12 15:21:52 -0400

    Added CheckAnswer body

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,19 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
     return (
         <div>
             <h3>Check Answer</h3>
+
+            <input
+                type="text"
+                value={userAnswer}
+                onChange={(event) => setUserAnswer(event.target.value)}
+            />
+
+            <span>{userAnswer === expectedAnswer ? "✔️" : "❌"}</span>
         </div>
     );
 }
