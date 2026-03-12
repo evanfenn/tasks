e6bbdb3bc642a63f97098bb392c5bbcfbbed5cdd
evanfenn <evanfenn@udel.edu>
2026-03-12 16:03:52 -0400

    Added body to MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selectedChoice, setSelectedChoice] =useState<string>(options[0]);
+    
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+
+            <select
+                value={selectedChoice}
+                onChange={(event) => {
+                    setSelectedChoice(event.target.value);
+                }}
+            >
+                {options.map((option: string) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+
+            <span>{selectedChoice === expectedAnswer ? "✔️" : "❌"}
+            </span>
         </div>
     );
 }
